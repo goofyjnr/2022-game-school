42021632f9e6f73127d37f0f80415f460464f380
goofyjnr <51934302+goofyjnr@users.noreply.github.com>
2022-05-12 09:55:59 +1200

    moving object

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ pygame.init()
 
 window = pygame.display.set_mode((WINDOW_WITDTH,WINDOW_HEIGHT))
 
+#create an image object
+#picture = Drawable((WINDOW_WITDTH/2,WINDOW_HEIGHT/2),100,100)
+
+#moveing object
+moving_object = Physics((0,WINDOW_HEIGHT/2),100,100)
+moving_object.vel = Vector2(4,0)
+
 
 #main game loop
 running = True
@@ -21,11 +28,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
+    moving_object.update()
     window.fill((70,60,78))
+    window.blit(moving_object.image,moving_object.rect)
     pygame.display.update()
     
-
-
-
 
 pygame.quit()
--- objects.py
+++ objects.py
@@ -1,18 +1,26 @@
 from pygame.sprite import Sprite
 from pygame.math import Vector2
 from pygame.image import load
+from pygame.transform import scale
 
 class Drawable(Sprite):
-    def __init__(self,position, width,height,image):
-        super().__init__(self)
+    def __init__(self,position, width,height,image="assets/chara-1.png"):
+        super().__init__()
 
         self.position = Vector2(position)
-        self.image = load(image)
-        self.rect = self.image.get_rect()
-    pass
+        self.image = scale(load(image),(width,height))
+        
+        self.rect = self.image.get_rect(midbottom=position)
 
 class Physics(Drawable):
-    pass
+    def __init__(self, position, width, height, image="assets/chara-1.png"):
+        super().__init__(position, width, height, image)
+        
+        self.vel = Vector2((0,0))
+
+    def update(self):
+        self.position += self.vel 
+        self.rect.midbottom = self.position
 
 class Player(Physics):
     pass
